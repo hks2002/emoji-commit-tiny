0e8e3e5486e4e79d8b541f92b8e31223696a03dc
Robert Huang <56649783@qq.com>
2026-01-18 18:08:59 +0800

    fix(logger): 🐛fix bug

--- src/logger.js
+++ src/logger.js
@@ -2,10 +2,11 @@
  * @Author                : Robert Huang<56649783@qq.com>                     *
  * @CreatedDate           : 2023-02-04 10:59:15                               *
  * @LastEditors           : Robert Huang<56649783@qq.com>                     *
- * @LastEditDate          : 2026-01-18 14:44:55                               *
+ * @LastEditDate          : 2026-01-18 18:06:38                               *
  * @FilePath              : emoji-commit-tiny/src/logger.js                   *
  * @CopyRight             : MerBleueAviation                                  *
  *****************************************************************************/
+
 
 import * as vscode from 'vscode'
 import packageJson from '../package.json'
@@ -69,7 +70,7 @@ export class Logger {
       return
     }
     this.logMessage('\u001b[35m' + message + '\u001b[0m', 'WARN')
-    window.showWarningMessage(message)
+    vscode.window.showWarningMessage(message)
     if (data) {
       this.logObject(data)
     }
@@ -80,7 +81,7 @@ export class Logger {
       return
     }
     this.logMessage('\u001b[31m' + message + '\u001b[0m', 'ERROR')
-    window.showErrorMessage(message)
+    vscode.window.showErrorMessage(message)
 
     if (typeof error === 'string') {
       // Errors as a string usually only happen with
